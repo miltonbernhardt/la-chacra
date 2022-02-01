c841251810d281d1e25224d336c3c4d98b525e8b
Milton Bernhardt <miltonbernhardt@gmail.com>
2022-02-01 18:55:25 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Aplicación destinada al uso de la quesería La Chacra.
+
+---
+
+Desarrollada como parte del proyecto final de carrera.
---
+++ src/main/java/com/brikton/lachacra/LaChacraApplication.java
@@ -0,0 +1,11 @@
+package com.brikton.lachacra;
+
+import org.springframework.boot.SpringApplication;
+
+public class LaChacraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaChacraApplication.class, args);
+    }
+
+}
