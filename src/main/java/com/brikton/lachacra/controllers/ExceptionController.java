f90268a0943451d20d81c86ced56ac071fbb293f
Milton Bernhardt <milton.bernhardt@mercadolibre.com>
2022-02-05 13:48:35 -0300

    InvalidLoteException deleted

--- src/main/java/com/brikton/lachacra/controllers/ExceptionController.java
+++ src/main/java/com/brikton/lachacra/controllers/ExceptionController.java
@@ -1,20 +1,13 @@
 package com.brikton.lachacra.controllers;
 
 import com.brikton.lachacra.exceptions.DatabaseException;
-import com.brikton.lachacra.exceptions.InvalidLoteException;
 import com.brikton.lachacra.exceptions.LoteNotFoundException;
 import com.brikton.lachacra.responses.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,19 +17,12 @@ import javax.servlet.http.HttpServletRequest;
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
     private final String msgInternalServerError = "Ocurrió un error interno";
-    private final String msgInvalidLote = "Los datos enviados no son correctos";
     private final String msgLoteNotFound = "Lote no encontrado";
 
     @ExceptionHandler(DatabaseException.class)
     protected ResponseEntity<ErrorResponse> handlerDatabaseException(HttpServletRequest req, DatabaseException ex) {
         log.error("Request: {} - {}", req.getRequestURL(), ex);
         return ResponseEntity.internalServerError().body(ErrorResponse.set(msgInternalServerError));
-    }
-
-    @ExceptionHandler(InvalidLoteException.class)
-    protected ResponseEntity<ErrorResponse> handlerInvalidLoteException(HttpServletRequest req, InvalidLoteException ex) {
-        log.error("Request: {} - {}", req.getRequestURL(), ex);
-        return ResponseEntity.unprocessableEntity().body(ErrorResponse.set(msgInvalidLote, ex.getInvalidFields()));
     }
 
     @ExceptionHandler(LoteNotFoundException.class)
