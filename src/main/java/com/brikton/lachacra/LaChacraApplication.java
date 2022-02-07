e24591983135b72cc7814504b2f5edd6bd218e9c
Milton Bernhardt <milton.bernhardt@mercadolibre.com>
2022-02-07 16:28:05 -0300

    corso origin added

--- src/main/java/com/brikton/lachacra/LaChacraApplication.java
+++ src/main/java/com/brikton/lachacra/LaChacraApplication.java
@@ -2,6 +2,9 @@ package com.brikton.lachacra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 //@EnableScheduling
@@ -11,4 +14,13 @@ public class LaChacraApplication {
         SpringApplication.run(LaChacraApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
