d4f89684ec41b87bdbe5da56f0f1e5b5851cbe6e
Milton Bernhardt <milton.bernhardt@mercadolibre.com>
2022-03-01 17:07:25 -0300

    PrecioService was refactored

--- src/main/java/com/brikton/lachacra/repositories/PrecioRepository.java
+++ src/main/java/com/brikton/lachacra/repositories/PrecioRepository.java
@@ -18,7 +18,8 @@ public interface PrecioRepository extends JpaRepository<Precio, Long> {
     @Query("SELECT p.id FROM Precio p WHERE p.queso.id = :idQueso")
     List<Long> findAllByIdQueso(Long idQueso);
 
-    boolean existsByQuesoAndTipoCliente(Queso queso, TipoCliente tipoCliente);
+    @Query("SELECT CASE WHEN count(*) > 0 THEN true ELSE false END FROM Precio p WHERE p.queso.id=:idQueso AND p.tipoCliente.id=:idTipoCliente")
+    boolean existsByQuesoAndTipoCliente(Long idQueso, Long idTipoCliente);
 
     @Query("SELECT CASE WHEN count(*) > 0 THEN true ELSE false END FROM Precio p WHERE p.queso.id=:idQueso AND p.tipoCliente.id=:idTipoCliente AND p.id=:id")
     boolean existsByIdAndQuesoAndTipoCliente(Long id, Long idQueso, Long idTipoCliente);
--- src/main/java/com/brikton/lachacra/services/PrecioService.java
+++ src/main/java/com/brikton/lachacra/services/PrecioService.java
@@ -3,6 +3,7 @@ package com.brikton.lachacra.services;
 import com.brikton.lachacra.dtos.PrecioDTO;
 import com.brikton.lachacra.dtos.PrecioUpdateDTO;
 import com.brikton.lachacra.entities.Precio;
+import com.brikton.lachacra.entities.Queso;
 import com.brikton.lachacra.entities.TipoCliente;
 import com.brikton.lachacra.exceptions.*;
 import com.brikton.lachacra.repositories.PrecioRepository;
@@ -38,61 +39,70 @@ public class PrecioService {
     }
 
     public PrecioDTO save(PrecioDTO dto) throws PrecioAlreadyExistsException {
+        validateExistencePrecio(dto.getIdQueso(), dto.getIdTipoCliente());
         var precio = precioFromDTO(dto);
-
-        if (repository.existsByQuesoAndTipoCliente(precio.getQueso(), precio.getTipoCliente()))
-            throw new PrecioAlreadyExistsException();
-
         precio = repository.save(precio);
         return new PrecioDTO(precio);
     }
+
+    private void validateExistencePrecio(Long idQueso, Long idTipoCliente) {
+        if (repository.existsByQuesoAndTipoCliente(idQueso, idTipoCliente))
+            throw new PrecioAlreadyExistsException();
+    }
+
+    public PrecioDTO update(PrecioUpdateDTO dto) throws PrecioNotFoundException {
+        validateExistencePrecio(dto.getId(), dto.getIdQueso(), dto.getIdTipoCliente());
+        var precio = precioFromDTO(new PrecioDTO(dto)); //todo test exceptions
+        precio = repository.save(precio);
+        return new PrecioDTO(precio);
+    }
+
+    private void validateExistencePrecio(Long id, Long idQueso, Long idTipoCliente) {
+        if (!repository.existsByIdAndQuesoAndTipoCliente(id, idQueso, idTipoCliente))
+            throw new PrecioNotFoundException();
+    }
+
 
     private Precio precioFromDTO(PrecioDTO dto) throws TipoClienteNotFoundConflictException, QuesoNotFoundConflictException {
         Precio precio = new Precio();
 
-        TipoCliente tipoCliente;
+        var tipoCliente = getTipoCliente(dto.getIdTipoCliente());
+        precio.setTipoCliente(tipoCliente);
+
+        var queso = getQueso(dto.getIdQueso());
+        precio.setQueso(queso);
+
+        precio.setValor(dto.getValor());
+        precio.setId(dto.getId());
+        return precio;
+    }
+
+    private Queso getQueso(Long id) {
+        var queso = quesoRepository.findById(id);
+        if (queso.isPresent() && queso.get().getFechaBaja() == null)
+            return queso.get();
+        throw new QuesoNotFoundConflictException();
+    }
+
+    private TipoCliente getTipoCliente(Long id) {
         try {
-            tipoCliente = tipoClienteService.getEntity(dto.getIdTipoCliente());
+            return tipoClienteService.get(id);
         } catch (TipoClienteNotFoundException e) {
             throw new TipoClienteNotFoundConflictException();
         }
-
-        var queso = quesoRepository.findById(dto.getIdQueso());
-        if (queso.isEmpty())
-            throw new QuesoNotFoundConflictException();
-
-        precio.setTipoCliente(tipoCliente);
-        precio.setQueso(queso.get());
-        precio.setValor(dto.getValor());
-        precio.setId(dto.getId());
-        return precio;
-    }
-
-    public PrecioDTO update(PrecioUpdateDTO dto) throws PrecioNotFoundException {
-        if (!repository.existsByIdAndQuesoAndTipoCliente(dto.getId(), dto.getIdQueso(), dto.getIdTipoCliente()))
-            throw new PrecioNotFoundException();
-
-        var precio = precioFromDTO(dto);
-        precio = repository.save(precio);
-        return new PrecioDTO(precio);
-    }
-
-
-    private Precio precioFromDTO(PrecioUpdateDTO dto) {
-        var precio = new Precio();
-
-        var tipoCliente = tipoClienteService.getEntity(dto.getIdTipoCliente());
-        var queso = quesoRepository.getById(dto.getIdQueso());
-
-        precio.setTipoCliente(tipoCliente);
-        precio.setQueso(queso);
-        precio.setValor(dto.getValor());
-        precio.setId(dto.getId());
-        return precio;
     }
 
     public void deletePreciosByQueso(Long idQueso) {
         var allPrecioIDs = repository.findAllByIdQueso(idQueso);
         repository.deleteAllById(allPrecioIDs);
+    }
+
+    public Double getPrecioValue(Queso queso, TipoCliente tipoCliente) {
+        var precio = repository.findByQuesoAndAndTipoCliente(queso, tipoCliente);
+
+        if (precio.isEmpty())
+            throw new PrecioNotFoundException();
+
+        return precio.get().getValor();
     }
 }
