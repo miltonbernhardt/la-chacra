3f3bd1e76759ef6d00192f285adcf74760294bff
Milton Bernhardt <milton.bernhardt@mercadolibre.com>
2022-03-26 23:36:11 -0300

    annotations of privileges were added

---
+++ src/main/java/com/brikton/lachacra/annotations/HasVerVentasAuthority.java
@@ -0,0 +1,16 @@
+
+package com.brikton.lachacra.annotations;
+
+import com.brikton.lachacra.constants.Path;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = {ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('" + Path.VER_VENTAS + "')")
+public @interface HasVerVentasAuthority {
+}
