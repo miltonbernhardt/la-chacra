679cc54065ee2b1742db241c06b79bea263e5d83
Elias <eliasbrizi@yahoo.es>
2022-04-02 12:13:31 -0300

    tests

--- src/test/java/com/brikton/lachacra/services/EmbalajeServiceTest.java
+++ src/test/java/com/brikton/lachacra/services/EmbalajeServiceTest.java
@@ -1,15 +1,22 @@
 package com.brikton.lachacra.services;
 
+import com.brikton.lachacra.constants.ErrorMessages;
 import com.brikton.lachacra.dtos.EmbalajeDTO;
 import com.brikton.lachacra.dtos.QuesoDTO;
+import com.brikton.lachacra.entities.Embalaje;
 import com.brikton.lachacra.entities.Queso;
+import com.brikton.lachacra.entities.TipoEmbalaje;
+import com.brikton.lachacra.exceptions.EmbalajeAlreadyExistsException;
 import com.brikton.lachacra.repositories.EmbalajeRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.AdditionalAnswers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -28,6 +35,66 @@ public class EmbalajeServiceTest {
     QuesoService quesoService;
 
     @Test
+    public void Get_All__OK(){
+        when(repository.findAll()).thenReturn(List.of(mockEmbalaje()));
+        var result = service.getAll();
+        assertEquals(1,result.size());
+    }
+
+    @Test
+    public void Get__OK(){
+        when(repository.findById(any())).thenReturn(Optional.of(mockEmbalaje()));
+        var result = service.get(1L);
+        assertEquals(1L,result.getId());
+    }
+
+    @Test
+    @Disabled
+    public void Save_New__OK(){
+        when(repository.existsById(1L)).thenReturn(false);
+        when(repository.existsByTipoEmbalajeAndListaQuesosContains(any(),any()))
+                .thenReturn(false);
+        when(repository.save(any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+        var result = service.saveNew(mockDTO());
+        assertEquals(1L,result.getId());
+    }
+
+    @Test
+    public void Save_New__Embalaje_already_exists(){
+        when(repository.existsById(1L)).thenReturn(false);
+         when(repository.existsByTipoEmbalajeAndListaQuesosContains(any(),any()))
+                .thenReturn(true);
+
+        EmbalajeAlreadyExistsException result = assertThrows(
+                EmbalajeAlreadyExistsException.class,() -> service.saveNew(mockDTO())
+        );
+        assertEquals(ErrorMessages.MSG_EMBALAJE_ALREADY_EXISTS,result.getMessage());
+    }
+
+    @Test
+    public void Save_New__id_already_exists(){
+        when(repository.existsById(1L)).thenReturn(true);
+
+        EmbalajeAlreadyExistsException result = assertThrows(
+                EmbalajeAlreadyExistsException.class,() -> service.saveNew(mockDTO())
+        );
+        assertEquals(ErrorMessages.MSG_EMBALAJE_ALREADY_EXISTS,result.getMessage());
+    }
+
+    @Test
+    public void Update_Stock__OK(){
+        var embalaje = mockEmbalaje();
+        embalaje.setStock(50);
+
+        when(repository.findById(any())).thenReturn(Optional.of(embalaje));
+        when(repository.save(any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+        var result = service.updateStock(1L,50);
+        assertEquals(100,result.getStock());
+    }
+
+    @Test
     public void Embalaje_From_DTO__OK(){
         var dto = mockDTO();
 
@@ -40,6 +107,18 @@ public class EmbalajeServiceTest {
         var resultDTO = new EmbalajeDTO(result);
         assertEquals(1L,resultDTO.getId());
         assertEquals(2,resultDTO.getListaQuesos().size());
+        assertEquals(TipoEmbalaje.CAJA,result.getTipoEmbalaje());
+    }
+
+    Embalaje mockEmbalaje(){
+        var embalaje = new Embalaje();
+        embalaje.setTipoEmbalaje(TipoEmbalaje.CAJA);
+        embalaje.setStock(500);
+        var quesoA =mockQuesoA();
+        var quesoB = mockQuesoB();
+        embalaje.setListaQuesos(List.of(quesoA,quesoB));
+        embalaje.setId(1L);
+        return embalaje;
     }
 
     EmbalajeDTO mockDTO(){
