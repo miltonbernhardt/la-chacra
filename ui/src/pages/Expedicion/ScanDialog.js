c3be88cf7f92c826799fed2a0956e7edd2728553
Elias <eliasbrizi@yahoo.es>
2022-04-22 08:45:02 -0300

    scandialog yet to test

--- ui/src/pages/Expedicion/ScanDialog.js
+++ ui/src/pages/Expedicion/ScanDialog.js
@@ -112,6 +112,7 @@ export const ScanDialog = ({ open, onClose, onSubmit, clientes, cliente, fechaEx
         onClose();
     }, [onClose])
 
+    //TODO test with scanner
     return (
         <>
             <BarcodeReader
@@ -182,6 +183,8 @@ export const ScanDialog = ({ open, onClose, onSubmit, clientes, cliente, fechaEx
                                         })}
                                     </Stack>
                                 </Grid>
+
+                                {/* //TODO test with scanner */}
                                 <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
                                     <FormControlLabel
                                         label="Lote Completo"
